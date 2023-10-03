efda5c929cf75ea1e79f3eb7689cd96b0a77aaed
jeff-ooi <jeffooi@tamu.edu>
2023-10-03 18:58:16 -0500

    updates shifts generation code with comments

--- generate_shifts.py
+++ generate_shifts.py
@@ -21,48 +21,25 @@ with open("./simulation/employee_shift.csv", "w", newline='') as employeeShiftJu
     # all 12 months
     while month < 13:
         day_of_week = 1
-        
+        # all 7 days
         while day_of_week < 8:
             for i in range(len(employee_ids)):
                 employee = i+1
-                # chosses a random shift that starts before or on 13
+                # chooses a random shift that starts before or on 13
                 shift = random.randrange(0,3)
+                # each employee works 3 shifts
                 for i in range(3):
+                    # third iteration is to write the second iteration's shift
                     employeeShiftJunctionWriter.writerow([id,shift,employee,month,day_of_week])
+                    # chooses a random shift that is later than shifts already worked
                     if i < 3-2:
+                        # chooses a random shift that is later than the shift already worked, but not all the way until the final shift
                         shift = random.randrange(shift+1,shift+3+1)
                     else:
+                        # last second goes until the final shift
                         shift = random.randrange(shift,8+1)
                     id += 1
                 
             day_of_week += 1
         
         month += 1
-exit(0)
-with open("./simulation/shifts.csv", "w", newline='') as shiftsFile:
-    shiftsWriter = csv.writer(shiftsFile)
-    shiftsWriter.writerow(["employee_id","start_time","end_time","day_of_week","month"])    # header
-    
-    id = 1
-    # manager is employee_id 1, works 11-21 every day (rip)
-    for i in range(12):
-        for j in range(7):
-            shiftsWriter.writerow([id,11,9+12,j+1,i+1])
-    
-    id += 1
-    while id < 6:
-        month = 1
-        
-        while month < 13:
-            day_of_week = 1
-            
-            while day_of_week < 8:
-                start_time = random.randrange(11,9+12+1-5)  # random start_time
-                end_time = random.randrange(start_time+1,9+12+1)    # random end_time > start_time
-                # end_time = 9+12     # each employee works until the shop closes
-                shiftsWriter.writerow([id,start_time,end_time,day_of_week,month])
-                day_of_week += 1
-            
-            month += 1
-        
-        id += 1
