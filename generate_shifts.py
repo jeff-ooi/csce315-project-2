7d44b5eca50b714dda0fc2ab2a3917c9803edf28
jeff-ooi <jeffooi@tamu.edu>
2023-10-03 18:55:00 -0500

    created a better way to track shifts, generated new shift data

--- generate_shifts.py
+++ generate_shifts.py
@@ -1,6 +1,44 @@
 import random
 import csv
 
+shifts = [0,1,2,3,4,5,6,7,8]
+employee_ids = [1,2,3,4,5]
+
+# clears the file and writes the header
+with open("./simulation/employee_shift.csv", "w", newline='') as employeeShiftJunctionFile:
+    employeeShiftJunctionWriter = csv.writer(employeeShiftJunctionFile)
+    employeeShiftJunctionWriter.writerow(["id","shift_id","employee_id","month","day_of_week"])    # header
+    
+    id = 0
+    # manager is employee_id 1, works 11-21 every day (rip)
+    for month in range(12):
+        for day in range(7):
+            for shift in range(len(shifts)):
+                employeeShiftJunctionWriter.writerow([id,shift,1,month+1,day+1])
+                
+    id += 1
+    month = 1
+    # all 12 months
+    while month < 13:
+        day_of_week = 1
+        
+        while day_of_week < 8:
+            for i in range(len(employee_ids)):
+                employee = i+1
+                # chosses a random shift that starts before or on 13
+                shift = random.randrange(0,3)
+                for i in range(3):
+                    employeeShiftJunctionWriter.writerow([id,shift,employee,month,day_of_week])
+                    if i < 3-2:
+                        shift = random.randrange(shift+1,shift+3+1)
+                    else:
+                        shift = random.randrange(shift,8+1)
+                    id += 1
+                
+            day_of_week += 1
+        
+        month += 1
+exit(0)
 with open("./simulation/shifts.csv", "w", newline='') as shiftsFile:
     shiftsWriter = csv.writer(shiftsFile)
     shiftsWriter.writerow(["employee_id","start_time","end_time","day_of_week","month"])    # header
