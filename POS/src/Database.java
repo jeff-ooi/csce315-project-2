bf7c287a2e8d39b8a3356213ec089db8dde010d5
jeff-ooi <jeffooi@tamu.edu>
2023-10-07 21:56:28 -0500

    ensured that the functions actually work

--- POS/src/Database.java
+++ POS/src/Database.java
@@ -1,14 +1,14 @@
-import java.rmi.server.ExportException;
+// import java.rmi.server.ExportException;
 import java.sql.*;
 // import java.util.Scanner;
 
 public class Database {
 
     private Connection conn;
-    final String teamName = "08m";
-    final String dbName = "csce315331_" + teamName + "_db";
-    final String dbConnectionString = "jdbc:postgresql://csce-315-db.engr.tamu.edu/" + dbName;
-    Statement createStatement;
+    final private String teamName = "08m";
+    final private String dbName = "csce315331_" + teamName + "_db";
+    final private String dbConnectionString = "jdbc:postgresql://csce-315-db.engr.tamu.edu/" + dbName;
+    private Statement createStatement;
 
     public Database() {
         conn = null;
@@ -50,11 +50,12 @@ public class Database {
         try {
             // why doesn't java have raw strings?????
             // newEmployee = createStatement.executeQuery(
-            createStatement.executeQuery(
+            createStatement.execute(
                 "INSERT INTO employee (id, username, password, name, start_date, salary, position) VALUES (" +
-                id + ", \'" + userName + "\', \'" + password + "\', \'" + name + "\', \'" + "\', \'" + startDate +
+                id + ", \'" + userName + "\', \'" + password + "\', \'" + name + "\', \'" + startDate +
                 "\', " + salary + ", \'" + position + "\');"
             );
+            System.out.println("Successfully added Employee " + id);
         }
         catch (Exception e) {
             System.out.println("Failed to add Employee");
@@ -65,9 +66,10 @@ public class Database {
 
     public void deleteEmployee(int id) {
         try {
-            createStatement.executeQuery(
+            createStatement.execute(
                 "DELETE FROM employee WHERE id = " + id + ";"
             );
+            System.out.println("Successfully deleted Employee " + id);
         }
         catch (Exception e) {
             System.out.println("Failed to delete Employee " + id);
@@ -82,10 +84,11 @@ public class Database {
         // ResultSet newInventoryItem = null;
         try {
             // newInventoryItem = createStatement.executeQuery(
-            createStatement.executeQuery(
+            createStatement.execute(
                 "INSERT INTO inventory (id, name, last_restock_date, amount_remaining, amount_used) VALUES (" +
                 id + ", \'" + name + "\', \'" + lastRestockDate + "\', " + amountRemaining + ", " + amountUsed + ");"
             );
+            System.out.println("Successfully added Inventory Item " + id);
         }
         catch (Exception e) {
             System.out.println("Failed to add Inventory Item");
@@ -96,9 +99,10 @@ public class Database {
 
     public void deleteInventoryItem(int id) {
         try {
-            createStatement.executeQuery(
+            createStatement.execute(
                 "DELETE FROM inventory WHERE id = " + id + ";"
             );
+            System.out.println("Successfully deleted Inventory Item " + id);
         }
         catch (Exception e) {
             System.out.println("Failed to delete Inventory Item " + id);
@@ -106,33 +110,60 @@ public class Database {
         }
     }
 
-    public void updateInventoryItemAmount(int id, int newAmountRemaining) {
+    public void updateInventoryItemAmountRemaining(int id, int newAmountRemaining) {
         try {
-            ResultSet inventoryItem = createStatement.executeQuery(
-                "SELECT * FROM inventory WHERE id = " + id + ";"
-            );
-            int currentAmountRemaining = inventoryItem.getInt("amount_remaining");
-            int currentAmountUsed = inventoryItem.getInt("amount_used");
-            createStatement.executeQuery(
+            // ResultSet inventoryItem = createStatement.executeQuery(
+            //     "SELECT * FROM inventory WHERE id = " + id + ";"
+            // );
+            // inventoryItem.next();
+            // int currentAmountRemaining = inventoryItem.getInt("amount_remaining");
+            // System.out.println("Got amount_remaining");
+            // int currentAmountUsed = inventoryItem.getInt("amount_used");
+            // System.out.println("Got amount_used");
+            // System.out.println(
+            //     "UPDATE inventory " +
+            //     "SET amount_remaining = " + newAmountRemaining + ", " + 
+            //     "amount_used = " + (currentAmountUsed + (currentAmountRemaining - newAmountRemaining)) + " " +
+            //     "WHERE id = " + id + ";"
+            // );
+            createStatement.execute(
                 "UPDATE inventory " +
-                "SET amount_remaning = " + newAmountRemaining + ", " + 
-                "SET amount_used = " + (currentAmountUsed + (currentAmountRemaining - newAmountRemaining)) + " " +
+                "SET amount_remaining = " + newAmountRemaining + " " + 
+                // "amount_used = " + (currentAmountUsed + (currentAmountRemaining - newAmountRemaining)) + " " +
                 "WHERE id = " + id + ";"
             );
+            // System.out.println("Successfully updated Inventory Item " + id + " amounts");
+            System.out.println("Successfully updated Inventory Item " + id + " amount remaining");
         }
         catch (Exception e) {
-            System.out.println("Failed to update Inventory Item " + id + " Amount Remaining");
+            System.out.println("Failed to update Inventory Item " + id + " Amount remaining");
+            e.printStackTrace();
+        }
+    }
+
+    public void updateInventoryItemAmountUsed(int id, int newAmountUsed) {
+        try {
+            createStatement.execute(
+                "UPDATE inventory " +
+                "SET amount_used = " + newAmountUsed + " " +
+                "WHERE id = " + id + ";"
+            );
+            System.out.println("Successfully updated Inventory Item " + id + " amount used");
+        }
+        catch (Exception e) {
+            System.out.println("Failed to update Inventor Item " + id + " Amount used");
             e.printStackTrace();
         }
     }
 
     public void updateInventoryItemName(int id, String newName) {
         try {
-            createStatement.executeQuery(
+            createStatement.execute(
                 "UPDATE inventory " +
-                "SET name = \'" + newName + "\'' " +
+                "SET name = \'" + newName + "\' " +
                 "WHERE id = " + id + ";"
             );
+            System.out.println("Successfully updated Inventory Item " + id + " name");
         }
         catch (Exception e) {
             System.out.println("Failed to update Inventory Item " + id + " name");
@@ -145,13 +176,15 @@ public class Database {
             ResultSet inventoryItem = createStatement.executeQuery(
                 "SELECT * FROM inventory WHERE id = "+ id + ";"
             );
-            int amountRemaining = inventoryItem.getInt("amount_remaning");
-            createStatement.executeQuery(
+            inventoryItem.next();
+            int amountRemaining = inventoryItem.getInt("amount_remaining");
+            createStatement.execute(
                 "UPDATE inventory " + 
                 "SET amount_remaining = " + (amountRemaining + restockAmount) + ", " + 
-                "SET last_restock_date = " + restockDate + " " + 
+                "last_restock_date = \'" + restockDate + "\' " + 
                 "WHERE id = " + id + ";"
             );
+            System.out.println("Successfully restocked Inventory Item " + id);
         }
         catch (Exception e) {
             System.out.println("Failed to restock Inventory Item " + id);
@@ -166,10 +199,11 @@ public class Database {
         // ResultSet newMenuItem = null;
         try {
             // newMenuItem = createStatement.executeQuery(
-            createStatement.executeQuery(
+            createStatement.execute(
                 "INSERT INTO menu (id, name, price) VALUES (" +
                 id + ", \'" + name + "\', " + price + ");"
             );
+            System.out.println("Successfully added Menu Item " + id);
         }
         catch (Exception e) {
             System.out.println("Failed to add Menu Item");
@@ -180,9 +214,10 @@ public class Database {
 
     public void deleteMenuItem(int id) {
         try {
-            createStatement.executeQuery(
+            createStatement.execute(
                 "DELETE FROM menu WHERE id = " + id + ";"
             );
+            System.out.println("Successfully deleted Menu Item " + id);
         }
         catch (Exception e) {
             System.out.println("Failed to delete Menu Item " + id);
@@ -193,11 +228,12 @@ public class Database {
     // updates the price of a Menu Item
     public void updateMenuItemPrice(int id, double newPrice) {
         try {
-            createStatement.executeQuery(
+            createStatement.execute(
                 "UPDATE menu " +
                 "SET price = " + newPrice + " " +
                 "WHERE id = " + id + ";"
             );
+            System.out.println("Successfully updated Menu Item " + id + " price");
         }
         catch (Exception e) {
             System.out.println("Faled to update Menu Item " + id + " price");
@@ -207,11 +243,12 @@ public class Database {
 
     public void updateMenuItemName(int id, String newName) {
         try {
-            createStatement.executeQuery(
+            createStatement.execute(
                 "UPDATE menu " + 
                 "SET name = \'" + newName + "\' " +
                 "WHERE id = " + id + ";" 
             );
+            System.out.println("Successfully updated Menu Item " + id + " name");
         }
         catch (Exception e) {
             System.out.println("Failed to update Menu Item " + id + " name");
